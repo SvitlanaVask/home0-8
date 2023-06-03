58b9bcfd5ef2aee85b9a9d4b9b6260f098ac5c80
SvitlanaVas <svetaklymchuk@gmail.com>
2023-06-03 18:43:20 +0300

    add 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCAL_TIME = "videoplayer-current-time";
+const currentTime = localStorage.getItem(LOCAL_TIME);
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+function onCurrentTime (currentTime) {
+    const time = currentTime.seconds;
+    localStorage.setItem(LOCAL_TIME, JSON.stringify(time));
+    
+}
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+const feedbackForm = document.querySelector('.feedback-form');
+
+feedbackForm.addEventListener('input', throttle(onFormInput, 250));
+feedbackForm.addEventListener('submit', onFormSubmit);
+
+onFormInit();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formData = new FormData(feedbackForm);
+  formData.forEach((value, name) => console.log(value, name));
+
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+  evt.currentTarget.reset();
+}
+
+function onFormInput(evt) {
+  let storageData =  localStorage.getItem(LOCALSTORAGE_KEY);
+
+  if (storageData) {
+    storageData = JSON.parse(storageData);
+  } else {
+    storageData = {};
+  }
+
+  storageData[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(storageData));
+}
+
+function onFormInit() {
+  let storageData = localStorage.getItem(LOCALSTORAGE_KEY);
+  if (storageData) {
+    storageData = JSON.parse(storageData);  
+    Object.entries(storageData).forEach(([name, value]) => feedbackForm.elements[name].value = value);
+  }
+}
 
